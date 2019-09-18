ff230fcb534ae01637055af60f09aa55747da157
zoyoy1203 <2071686148@qq.com>
2019-09-19 00:55:50 +0800

    熬了个夜，把数据库连接增删查写上去了

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var express = require('express');
+const mysql = require('mysql');
+
 
 //自定义模块todoController
 var todoController = require('./controller/todoController');
@@ -6,11 +8,24 @@ var todoController = require('./controller/todoController');
 
 var app = express();
 
+//创建数据库连接
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database: 'db_todoapp'
+})
+//Connect
+db.connect((err) => {
+    if(err) throw err;
+    console.log("Mysql Connect...");
+})
+
 app.set('view engine','ejs');
 
 //整个public模块化 在服务器中展示
 app.use(express.static('./public'));
 
-todoController(app);
+todoController(app,db);
 
 app.listen(3000);
--- controller/todoController.js
+++ controller/todoController.js
@@ -2,32 +2,51 @@ var bodyParser = require('body-parser');
 //对数据进行解析
 var urlencodeParser = bodyParser.urlencoded({extended:false});
 
-var data = [
-    {item:"好好学习！天天向上！"},
-    {item:"明天中午吃什么？"},
-    {item:"还有几节课就没了。。"}
-];
+// var data = [
+//     {item:"好好学习！天天向上！"},
+//     {item:"明天中午吃什么？"},
+//     {item:"还有几节课就没了。。"}
+// ];
 
-module.exports = function (app) {
+module.exports = function (app,db) {
     //获取数据
     app.get('/todo',function (req,res) {
-        res.render('todo',{todos:data});
+        // res.render('todo',{todos:data});
+        let sql = 'SELECT * FROM todo_text';
+        db.query(sql, (err, result) => {
+            if (err) throw err;
+            // console.log(result)
+            res.render('todo',{todos:result})
+        })
     });
     //传递数据
     app.post('/todo',urlencodeParser,function (req,res) {
-        data.push(req.body);
+        // data.push(req.body);
+        let text = req.body;
+        console.log(text);
+        let sql = "INSERT INTO todo_text SET ?";
+        db.query(sql, text, (err, result) => {
+            if(err) throw err;
+            console.log(result);
+        })
     });
     // 删除数据
-    app.delete('/todo/:item',function (req,res) {
+    app.delete('/todo/:id',function (req,res) {
         // console.log(req.params.item);
         // Todo.find({item:req.params.item}).remove(function (err,data) {
         //     if (err) throw err;
         //     res.json(data);
         // })
-        data = data.filter(function (todo) {
-            return req.params.item !== todo.item;
-        });
+        // data = data.filter(function (todo) {
+        //     return req.params.item !== todo.item;
+        // });
+        let sql = `DELETE FROM todo_text WHERE ID = ${req.params.id}`;
+        db.query(sql, (err, result) => {
+            if(err) throw err;
+            console.log(result);
+            res.json(result);
+        })
 
-        res.json(data);
+        // res.json(data);
     });
 }
