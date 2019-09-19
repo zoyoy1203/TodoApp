82d2cff9bf608dc663655f9d58e9734b4fa528b1
zoyoy1203 <2071686148@qq.com>
2019-09-19 16:46:52 +0800

    数据库文件，项目截图

--- db_todoapp.sql
+++ db_todoapp.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 80016
 File Encoding         : 65001
 
-Date: 2019-09-19 00:56:34
+Date: 2019-09-19 16:43:13
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -22,14 +22,15 @@ DROP TABLE IF EXISTS `todo_text`;
 CREATE TABLE `todo_text` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `text` varchar(255) NOT NULL,
+  `check` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of todo_text
 -- ----------------------------
-INSERT INTO `todo_text` VALUES ('1', '高级程序设计啦啦啦啦');
-INSERT INTO `todo_text` VALUES ('2', 'JavaScript设计模式');
-INSERT INTO `todo_text` VALUES ('3', '高性能MySQL');
-INSERT INTO `todo_text` VALUES ('8', '好好学习');
-INSERT INTO `todo_text` VALUES ('9', '哈哈哈写完了我');
+INSERT INTO `todo_text` VALUES ('2', 'JavaScript设计模式', '0');
+INSERT INTO `todo_text` VALUES ('8', '好好学习', '0');
+INSERT INTO `todo_text` VALUES ('10', '呵呵呵', '1');
+INSERT INTO `todo_text` VALUES ('11', '高性能MySQL', '1');
+INSERT INTO `todo_text` VALUES ('12', '大功告成！！！啦啦啦啦', '0');
